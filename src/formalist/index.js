dc70ef1e01b5cecfea3d334436b109a206a21260
Max Wheeler <max@makenosound.com>
2016-08-11 11:12:32 +1000

    Add an on-by-default option that *forces* a serialisation render when the parent `<form>` of our formalist component is submitted.

--- src/formalist/index.js
+++ src/formalist/index.js
@@ -10,6 +10,23 @@ import debounce from 'lodash.debounce'
 import 'ric'
 
 /**
+ * Recurse to find the first FORM that is a parent of `el`
+ * @param  {Node} el Child node to check parents of
+ * @return {Mixed} The parent node or false
+ */
+function findParentForm (el) {
+  const parentNode = el.parentNode
+  if (parentNode) {
+    if (parentNode.nodeName === 'FORM') {
+      return parentNode
+    } else {
+      return findParentForm(parentNode)
+    }
+  }
+  return false
+}
+
+/**
  * Simple wrapper to create the form outer
  */
 class FormWrapper extends Component {
@@ -46,6 +63,27 @@ class FormWrapper extends Component {
     )
   }
 
+  componentDidMount () {
+    const self = this
+    const {parentForm, form} = this.props
+    if (parentForm) {
+      // Ensure the serialize data get written before we submit
+      parentForm.addEventListener('submit', function onParentSubmit (e) {
+        e.preventDefault()
+        const formState = form.store.getState()
+        const serialized = self.serializeForm(formState)
+        self.setState({
+          formState,
+          serialized,
+        }, () => {
+          // After state is set, remove our listener and submit the form again
+          parentForm.removeEventListener('submit', onParentSubmit)
+          parentForm.submit()
+        })
+      })
+    }
+  }
+
   serializeForm (formState) {
     const {prefix} = this.props
     formState = formState || this.props.form.store.getState()
@@ -73,7 +111,12 @@ class FormWrapper extends Component {
 
 FormWrapper.propTypes = {
   form: PropTypes.object.isRequired,
+  parentForm: PropTypes.node,
   prefix: PropTypes.string,
+}
+
+const defaultOptions = {
+  serializeBeforeParentSubmit: true,
 }
 
 /**
@@ -82,7 +125,22 @@ FormWrapper.propTypes = {
  * @param  {AST} ast Formalist compatible abstract syntax tree
  */
 export default function formalist (el, props) {
-  let configuredTemplate = template(null, props.config)
-  let form = configuredTemplate(props.ast)
-  ReactDOM.render(<FormWrapper form={form} prefix={props.prefix} />, el)
+  const options = Object.assign({}, defaultOptions, props)
+  const configuredTemplate = template(null, options.config)
+  const form = configuredTemplate(options.ast)
+  const wrapperProps = {
+    form,
+    prefix: options.prefix,
+  }
+  // Pass through the parent form if we want to bind the serialize
+  if (options.serializeBeforeParentSubmit) {
+    const parentForm = findParentForm(el)
+    if (parentForm) {
+      wrapperProps.parentForm = parentForm
+    }
+  }
+  ReactDOM.render(
+    <FormWrapper {...wrapperProps} />,
+    el
+  )
 }
