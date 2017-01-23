a4bb0a7e01e08cfd58d57330268d3fd32b4e4c15
Max Wheeler <max@makenosound.com>
2017-01-23 21:53:16 +1100

    Adjust font loading.

--- src/fonts-loaded/index.js
+++ src/fonts-loaded/index.js
@@ -7,16 +7,16 @@
 import 'fontfaceobserver'
 
 export default function fontsLoaded (fontsToCheck) {
+  var timeout = 1500
   var fontsLoaded = window.localStorage.getItem('fontsLoaded')
 
   if (!fontsLoaded) {
-    var loading = []
-    fontsToCheck.forEach((font) => {
-      const promise = new window.FontFaceObserver(font.family, {
+    var loading = fontsToCheck.map((font) => {
+      const observer = new window.FontFaceObserver(font.family, {
         weight: font.weight,
         style: font.style,
       })
-      loading.push(promise.check())
+      return observer.load(null, timeout)
     })
 
     Promise.all(loading).then(() => {
