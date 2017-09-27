e2cce0c9493e3e125c0d8eeee3e6325364dceab8
Max Wheeler <max@makenosound.com>
2017-09-28 07:51:14 +1000

    Use standalone prop-types package (has been removed from React)

--- src/formalist/index.js
+++ src/formalist/index.js
@@ -1,8 +1,8 @@
 import React, {
   Component,
-  PropTypes,
 } from 'react'
 import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 import template from 'formalist-standard-react'
 import serialize from 'formalist-serialize-react'
 
